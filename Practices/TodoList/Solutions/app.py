7b4ac767932bac03c26a48bf28c1420818c8f6ba
KRUSTstation <115158641+KRUSTstation@users.noreply.github.com>
2026-04-12 19:44:03 +0800

    adjusted TodoList solution

--- Practices/TodoList/Solutions/app.py
+++ Practices/TodoList/Solutions/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 ITEMS = []
 
-@app.route('/', methods = ['POST', 'GET'])
+@app.route('/', methods = ['GET'])
 def base():
     return render_template('base.html', items=ITEMS)
 
